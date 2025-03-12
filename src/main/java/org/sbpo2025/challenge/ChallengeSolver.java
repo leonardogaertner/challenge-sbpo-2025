167ab598abe3f9659b7c01c140c4df8848288007
Leonardo Gaertner <leonardo.gaertner@bovenau.com.br>
2025-03-12 12:35:48 -0300

    First Prototype

--- src/main/java/org/sbpo2025/challenge/ChallengeSolver.java
+++ src/main/java/org/sbpo2025/challenge/ChallengeSolver.java
@@ -1,15 +1,17 @@
 package org.sbpo2025.challenge;
 
-import org.apache.commons.lang3.time.StopWatch;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 public class ChallengeSolver {
-    private final long MAX_RUNTIME = 600000; // milliseconds; 10 minutes
+    private final long MAX_RUNTIME = 600000; // 10 minutes in milliseconds
 
     protected List<Map<Integer, Integer>> orders;
     protected List<Map<Integer, Integer>> aisles;
@@ -27,8 +29,90 @@ public class ChallengeSolver {
     }
 
     public ChallengeSolution solve(StopWatch stopWatch) {
-        // Implement your solution here
-        return null;
+        Set<Integer> selectedOrders = new HashSet<>();
+        Set<Integer> selectedAisles = new HashSet<>();
+        int totalUnitsPicked = 0;
+
+        // We will track the total available units in each aisle to avoid picking the same item too much.
+        int[] totalUnitsAvailable = new int[nItems];
+
+        // Greedily select orders and aisles
+        for (int orderIndex = 0; orderIndex < orders.size(); orderIndex++) {
+            Map<Integer, Integer> order = orders.get(orderIndex);
+            int orderTotal = order.values().stream().mapToInt(Integer::intValue).sum();
+
+            // If adding this order exceeds the upper bound, skip the order
+            if (totalUnitsPicked + orderTotal > waveSizeUB) {
+                continue;  // Skip this order if it can't be fit within the wave size
+            }
+
+            // Add this order to the selected set
+            selectedOrders.add(orderIndex);
+            totalUnitsPicked += orderTotal;
+
+            // Try to match aisles to this order
+            List<Integer> aislesThatCanFulfillOrder = findAislesForOrder(order);
+
+            // If we can match aisles, add them
+            for (Integer aisleIndex : aislesThatCanFulfillOrder) {
+                selectedAisles.add(aisleIndex);
+                // Mark the aisle as "used" by updating the available units
+                Map<Integer, Integer> aisle = aisles.get(aisleIndex);
+                for (Map.Entry<Integer, Integer> entry : aisle.entrySet()) {
+                    totalUnitsAvailable[entry.getKey()] += entry.getValue();
+                }
+            }
+
+            // Check if we have exceeded the upper limit of the wave size
+            if (totalUnitsPicked > waveSizeUB) {
+                // Backtrack if necessary by removing the last order and aisles if we exceed the wave size
+                selectedOrders.remove(orderIndex);
+                totalUnitsPicked -= orderTotal;
+
+                // Remove aisles for this order
+                for (Integer aisleIndex : aislesThatCanFulfillOrder) {
+                    selectedAisles.remove(aisleIndex);
+                }
+            }
+
+            // Check if we have satisfied the lower bound of the wave size
+            if (totalUnitsPicked >= waveSizeLB && totalUnitsPicked <= waveSizeUB) {
+                break;  // We have found a valid wave, we can stop
+            }
+        }
+
+        // If we don't meet the lower bound after trying to select orders, return null
+        if (totalUnitsPicked < waveSizeLB) {
+            System.out.println("Solution doesn't meet the lower bound for total units picked.");
+            return null;
+        }
+
+        // Return the solution
+        return new ChallengeSolution(selectedOrders, selectedAisles);
+    }
+
+    // Helper method to find aisles that can fulfill the given order
+    private List<Integer> findAislesForOrder(Map<Integer, Integer> order) {
+        List<Integer> aislesThatCanFulfillOrder = new ArrayList<>();
+        for (int aisleIndex = 0; aisleIndex < aisles.size(); aisleIndex++) {
+            Map<Integer, Integer> aisle = aisles.get(aisleIndex);
+            if (isAisleCompatible(aisle, order)) {
+                aislesThatCanFulfillOrder.add(aisleIndex);
+            }
+        }
+        return aislesThatCanFulfillOrder;
+    }
+
+    // Helper method to check if the aisle has enough stock to fulfill the order
+    private boolean isAisleCompatible(Map<Integer, Integer> aisle, Map<Integer, Integer> order) {
+        for (Map.Entry<Integer, Integer> entry : order.entrySet()) {
+            Integer itemIndex = entry.getKey();
+            Integer requiredQuantity = entry.getValue();
+            if (!aisle.containsKey(itemIndex) || aisle.get(itemIndex) < requiredQuantity) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /*
